cedba39195371d320344d936c648f290007d127a
figo_ywc <yangwc@figo.cn>
2017-02-08 14:48:16 +0800

    no message

---
+++ FGBaseClass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FGBaseClass
+//
+//  Created by Eric on 17/1/9.
+//  Copyright © 2017年 YangWeiCong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
